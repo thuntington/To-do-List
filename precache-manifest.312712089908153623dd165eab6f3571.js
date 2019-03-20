0ed2ee1b4862f037a87a6a5d9997eeb791beb616
thuntington <thuntington994@gmail.com>
2019-03-20 20:06:02 +0000

    Updates

---
+++ precache-manifest.312712089908153623dd165eab6f3571.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "15bb3101340a9b514097",
+    "url": "/to-do-list/static/js/runtime~main.9900d293.js"
+  },
+  {
+    "revision": "aab49c8f25976ca05fa2",
+    "url": "/to-do-list/static/js/main.e4552d46.chunk.js"
+  },
+  {
+    "revision": "c47bc882f3330f437b3b",
+    "url": "/to-do-list/static/js/2.0923de73.chunk.js"
+  },
+  {
+    "revision": "aab49c8f25976ca05fa2",
+    "url": "/to-do-list/static/css/main.35230029.chunk.css"
+  },
+  {
+    "revision": "d1764be3fa80d377fb97b0ff0c284e07",
+    "url": "/to-do-list/index.html"
+  }
+];
